fcbb70d7d1254267345c5aeca25d6ba7df28ca9f
cfviotti <caio@viotti.me>
2018-07-05 17:54:16 -0300

    Try to fix text scaling [WIP]

--- src/static/js/chart.js
+++ src/static/js/chart.js
@@ -218,17 +218,19 @@ function positionHexagon(hexagonGroup) {
 
 function addText(hexagonGroup) {
   hexagonGroup.append("foreignObject")
-    .attr('x', function(d, i) { return d.element.getBBox().x; })
-    .attr('y', function(d, i) { return d.element.getBBox().y; })
-    .attr('width', function(d, i) { return d.element.getBBox().width; })
-    .attr('height', function(d, i) { return d.element.getBBox().height; })
-    .attr('transform', function(d, i) { return `scale(${d.size})`})
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', 1)
+    .attr('height', 1)
     .attr('overflow', 'visible')
     .append('xhtml:div')
-      .attr("class", 'text-box')
-      .append('xhtml:p')
-        .attr('class', 'text')
-        .text((d) => {return d.token;})
+      .attr("class", 'text-box-wrapper')
+      .append('xhtml:div')
+        .attr("class", 'text-box')
+        .attr('style', function(d, i) { return `transform: scale(${d.size})`})
+        .append('xhtml:p')
+          .attr('class', 'text')
+          .text((d) => {return d.token;})
 }
 
 function showHexagonGroup(hexagonGroup) {
